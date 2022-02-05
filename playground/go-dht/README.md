8d9005048160d3743cbc72cb4ee58a999f672f24
hanabi1224 <harlowmoo@gmail.com>
2022-02-05 21:45:26 +0800

    id dist

---
+++ playground/go-dht/README.md
@@ -0,0 +1,4 @@
+```
+go mod vendor
+go test
+```
--- playground/go-dht/id_dist_test.go
+++ playground/go-dht/id_dist_test.go
@@ -1,30 +1,32 @@
-package main
+package kbucketfix
 
 import (
-	"encoding/json"
+	"testing"
 
 	"github.com/libp2p/go-libp2p-core/crypto"
 	"github.com/libp2p/go-libp2p-core/peer"
 	kb "github.com/libp2p/go-libp2p-kbucket"
 )
 
-func main() {
+func TestIdDistribution(t *testing.T) {
 	m := make(map[int]int)
 	a := GenId()
-	for i := 0; i < 1000; i++ {
+	total := 0
+	for i := 0; i < 100000; i++ {
 		b := GenId()
 		m[kb.CommonPrefixLen(a, b)] += 1
+		total += 1
 	}
-	println(ToJsonString(m))
+	const TARGET float64 = .75
+	const BIAS float64 = .01
+	percentage := float64(m[0]+m[1]) / float64(total)
+	if percentage < TARGET-BIAS || percentage > TARGET+BIAS {
+		t.Error(percentage)
+	}
 }
 
 func GenId() kb.ID {
 	privkey, _, _ := crypto.GenerateKeyPair(crypto.Ed25519, -1)
 	id, _ := peer.IDFromPrivateKey(privkey)
 	return kb.ConvertPeerID(id)
-}
-
-func ToJsonString(i interface{}) string {
-	bytes, _ := json.Marshal(i)
-	return string(bytes)
 }
