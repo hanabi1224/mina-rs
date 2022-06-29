d3be26c9ca07bbe7f20d965d98dfed0d39a01cb9
hanabi1224 <hanabi1224@users.noreply.github.com>
2022-06-29 09:12:55 +0000

    deploy: d90cd4b7792efc49bf66879a39b689faf712961c

--- rustdoc/mina_merkle/macros/sidebar-items.js
+++ rustdoc/mina_merkle/macros/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"mod":[["mina_hasher","Mina hasher"],["mina_signer","Mina signer"],["o1_utils",""]]};
+window.SIDEBAR_ITEMS = {"mod":[["mina_curves",""],["mina_hasher","Mina hasher"],["mina_signer","Mina signer"],["o1_utils",""]]};
